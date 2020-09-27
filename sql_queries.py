7352931e797a06e880f344989112121dbf34204a
johnhiroshi7 <kjohnhiroshi@gmail.com>
2020-09-27 11:11:08 +0900

    insert statemetn change

--- sql_queries.py
+++ sql_queries.py
@@ -69,9 +69,9 @@ VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
-	INSERT INTO users (user_id, first_name, last_name, gender, level) 
-	VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level
+INSERT INTO users (user_id, first_name, last_name, gender, level) 
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level
 """)
 
 song_table_insert = ("""
