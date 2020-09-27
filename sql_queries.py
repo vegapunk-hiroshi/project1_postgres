4e437846a8725292b9a7bba661a5dd0ea4696c7c
johnhiroshi7 <kjohnhiroshi@gmail.com>
2020-09-27 10:57:28 +0900

    insert statemetn change

--- sql_queries.py
+++ sql_queries.py
@@ -63,18 +63,15 @@ weekday int
 )
 """)
 
-# INSERT RECORDS
-# ON CONFLICT (song_id, artist_id) 
-# DO NOTHING;
-
 songplay_table_insert = ("""
 INSERT INTO songplays (start_time, user_id, level, song_id, artist_id , session_id , location, user_agent)
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
-INSERT INTO users (user_id, first_name, last_name, gender, level) 
-VALUES (%s, %s, %s, %s, %s)
+	INSERT INTO users (user_id, first_name, last_name, gender, level) 
+	VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level
 """)
 
 song_table_insert = ("""
